be5f0a7fd8ce135acfc5614a6d656b42e79458a2
arifozalp <arif.218.camalan@gmail.com>
2025-09-23 16:16:29 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: '.',
   plugins: [react()],
   base: '/portfolio/', // For GitHub Pages deployment
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html')
+    }
   }
 })
